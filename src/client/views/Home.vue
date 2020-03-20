aa45dceec92f85a83a2ce51d3169836d40f1263b
Admin <roma.ushpayev@gmail.com>
2020-03-20 01:32:58 -0700

    text animation

--- src/client/views/Home.vue
+++ src/client/views/Home.vue
@@ -3,8 +3,8 @@
     <NavbarHome :key="$route.fullPath" />
     <div class="content-row">
       <div class="content">
-        <h1>Real stocks. Real prices. Real time.</h1>
-        <h1>Zero risk.</h1>
+        <h1><span class="animation stocks">Real stocks.</span> <span class="animation prices">Real prices.</span>  <span class="animation time">Real time.</span> </h1>
+        <h1> <span class="animation risk">Zero risk.</span> </h1>
       </div>
       <div class="illustration">
         <img src="../../../public/illustr.svg" class="img" />
@@ -26,7 +26,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .home {
   background-color: white;
   height: 100vh;
@@ -50,6 +50,38 @@ export default {
 .illustration {
   width: 50%;
   justify-content: center;
+}
+
+.animation {
+  display: inline-block;
+  position: relative;
+  top: -20px;
+  opacity: 0;
+}
+
+.stocks {
+  animation: fadein 1.5s forwards;
+
+}
+.prices {
+  animation: fadein 1.5s 0.4s forwards;
+}
+.time {
+  animation: fadein 1.5s 0.8s forwards;
+}
+.risk {
+  animation: fadein 1.5s 1.2s forwards;
+}
+
+
+@keyframes fadein {
+  0% {
+    /* transform: translate(0,-20px); */
+  }
+  100% {
+    transform: translate(0,20px);
+    opacity: 1;
+  }
 }
 
 @media (max-width: 414px) {
